35cfa6cbeea5e64c8bbccfcdcca9c757461b616b
Md. Raiyanul Haque Raiyan <md.raiyanul.haque.raiyan@gmail.com>
2026-01-21 23:02:51 +0600

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.SwingUtilities;
-import ui.LoginFrame;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(LoginFrame::new);
-    }
-}
-
